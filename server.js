8ab42517b9f5ab6ba07d6ff238a133eaaf541d13
Jan Carbonell <jcllobet@gmail.com>
2022-03-14 09:29:09 +0100

    nodejs adaptation

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic(__dirname, {'index': ['index.html']}));
-app.listen(3000);
-console.log('Server running on port 3000');
